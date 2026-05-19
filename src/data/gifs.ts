ebe3f301c49098e5f215f35349633421b4368f0f
Krzyś <38500332+krzysziomek@users.noreply.github.com>
2026-05-19 21:01:20 +0200

    e tam

--- src/data/gifs.ts
+++ src/data/gifs.ts
@@ -8,7 +8,7 @@ export const gifs: GifEntry[] = [
   { url: "https://dzien.scooby.boo/fajniedzialek.gif", videoUrl: "https://dzien.scooby.boo/fajniedzialek.mp4" }, // Sunday
   { url: "https://dzien.scooby.boo/fajniedzialek.gif", videoUrl: "https://dzien.scooby.boo/fajniedzialek.mp4" }, // Monday
   { url: "https://dzien.scooby.boo/fajniedzialek.gif", videoUrl: "https://dzien.scooby.boo/fajniedzialek.mp4" }, // Tuesday
-  { url: "https://dzien.scooby.boo/fajniedzialek.gif", videoUrl: "https://dzien.scooby.boo/fajniedzialek.mp4" }, // Wednesday
+  { url: "https://dzien.scooby.boo/supersroda.gif", videoUrl: "https://dzien.scooby.boo/supersroda.mp4" }, // Wednesday
   { url: "https://dzien.scooby.boo/fajniedzialek.gif", videoUrl: "https://dzien.scooby.boo/fajniedzialek.mp4" }, // Thursday
   { url: "https://dzien.scooby.boo/fajniedzialek.gif", videoUrl: "https://dzien.scooby.boo/fajniedzialek.mp4" }, // Friday
   { url: "https://dzien.scooby.boo/fajniedzialek.gif", videoUrl: "https://dzien.scooby.boo/fajniedzialek.mp4" }, // Saturday
