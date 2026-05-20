a33dd828270dfcf973b2e402be3e82e86f4163be
Krzyś <38500332+krzysziomek@users.noreply.github.com>
2026-05-20 15:14:26 +0200

    Create kur.ts

---
+++ src/pages/kur.ts
@@ -0,0 +1,19 @@
+import type { APIRoute } from 'astro';
+import fs from 'node:fs';
+import path from 'node:path';
+import { getTodaysGif } from '../data/gifs';
+
+export const GET: APIRoute = async () => {
+  const gif = getTodaysGif();
+  const filename = gif.gifFile;
+  const filePath = path.resolve('public', filename);
+  const fileBuffer = fs.readFileSync(filePath);
+
+  return new Response(fileBuffer, {
+    status: 200,
+    headers: {
+      'Content-Type': 'image/gif',
+      'Content-Disposition': `attachment; filename="dzien.gif"`,
+    },
+  });
+};
