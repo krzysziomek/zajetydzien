cec1113295ea4e452e6ef88b4dc69a9fbc973464
Krzyś <38500332+krzysziomek@users.noreply.github.com>
2026-05-20 15:11:35 +0200

    Update gifs.ts

--- src/data/gifs.ts
+++ src/data/gifs.ts
@@ -2,16 +2,17 @@
 export interface GifEntry {
   url: string;
   videoUrl: string;
+  gifFile: string;
 }
 
 export const gifs: GifEntry[] = [
-  { url: "https://i.imgur.com/1TlBrfZ.gif", videoUrl: "https://dzien.scooby.boo/fajniedzialek.mp4" }, // Sunday
-  { url: "https://i.imgur.com/JUnOMHp.gif", videoUrl: "https://dzien.scooby.boo/fajniedzialek.mp4" }, // Monday
-  { url: "https://i.imgur.com/JUnOMHp.gif", videoUrl: "https://dzien.scooby.boo/fajniedzialek.mp4" }, // Tuesday
-  { url: "https://i.imgur.com/1TlBrfZ.mp4", videoUrl: "https://dzien.scooby.boo/supersroda.mp4" }, // Wednesday
-  { url: "https://i.imgur.com/1TlBrfZ.gif", videoUrl: "https://dzien.scooby.boo/fajniedzialek.mp4" }, // Thursday
-  { url: "https://i.imgur.com/1TlBrfZ.gif", videoUrl: "https://dzien.scooby.boo/fajniedzialek.mp4" }, // Friday
-  { url: "https://i.imgur.com/1TlBrfZ.gif", videoUrl: "https://dzien.scooby.boo/fajniedzialek.mp4" }, // Saturday
+  { url: "https://i.imgur.com/1TlBrfZ.gif", videoUrl: "https://dzien.scooby.boo/fajniedzialek.mp4", gifFile: "fajniedzialek.gif" }, // Sunday
+  { url: "https://i.imgur.com/JUnOMHp.gif", videoUrl: "https://dzien.scooby.boo/fajniedzialek.mp4", gifFile: "fajniedzialek.gif" }, // Monday
+  { url: "https://i.imgur.com/JUnOMHp.gif", videoUrl: "https://dzien.scooby.boo/fajniedzialek.mp4", gifFile: "fajniedzialek.gif" }, // Tuesday
+  { url: "https://i.imgur.com/1TlBrfZ.gif", videoUrl: "https://dzien.scooby.boo/supersroda.mp4",    gifFile: "fajniedzialek.gif" }, // Wednesday
+  { url: "https://i.imgur.com/1TlBrfZ.gif", videoUrl: "https://dzien.scooby.boo/fajniedzialek.mp4", gifFile: "fajniedzialek.gif" }, // Thursday
+  { url: "https://i.imgur.com/1TlBrfZ.gif", videoUrl: "https://dzien.scooby.boo/fajniedzialek.mp4", gifFile: "fajniedzialek.gif" }, // Friday
+  { url: "https://i.imgur.com/1TlBrfZ.gif", videoUrl: "https://dzien.scooby.boo/fajniedzialek.mp4", gifFile: "fajniedzialek.gif" }, // Saturday
 ];
 
 export function getTodaysGif(): GifEntry {
