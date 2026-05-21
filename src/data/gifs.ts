e9741decc5400fce2160c2b76db546e89b235a5b
Krzyś <38500332+krzysziomek@users.noreply.github.com>
2026-05-21 13:20:45 +0200

    Updated gifs.ts

--- src/data/gifs.ts
+++ src/data/gifs.ts
@@ -10,8 +10,8 @@ export const gifs: GifEntry[] = [
   { url: "https://i.imgur.com/1TlBrfZ.gif", videoUrl: "https://dzien.scooby.boo/fajniedzialek.mp4", gifFile: "fajniedzialek.gif" }, // Sunday
   { url: "https://i.imgur.com/JUnOMHp.gif", videoUrl: "https://dzien.scooby.boo/fajniedzialek.mp4", gifFile: "fajniedzialek.gif" }, // Monday
   { url: "https://i.imgur.com/JUnOMHp.gif", videoUrl: "https://dzien.scooby.boo/fajniedzialek.mp4", gifFile: "fajniedzialek.gif" }, // Tuesday
-  { url: "", videoUrl: "https://dzien.scooby.boo/supersroda.mp4", gifFile: "test.gif", localFile: "test.gif" }, // Wednesday
-  { url: "https://i.imgur.com/1TlBrfZ.gif", videoUrl: "https://dzien.scooby.boo/fajniedzialek.mp4", gifFile: "fajniedzialek.gif" }, // Thursday
+  { url: "https://dzien.scooby.boo/test.gif", videoUrl: "https://dzien.scooby.boo/supersroda.mp4", gifFile: "test.gif", localFile: "test.gif" }, // Wednesday
+  { url: "https://dzien.scooby.boo/test.gif", videoUrl: "https://dzien.scooby.boo/fajniedzialek.mp4", gifFile: "fajniedzialek.gif" }, // Thursday
   { url: "https://i.imgur.com/1TlBrfZ.gif", videoUrl: "https://dzien.scooby.boo/fajniedzialek.mp4", gifFile: "fajniedzialek.gif" }, // Friday
   { url: "https://i.imgur.com/1TlBrfZ.gif", videoUrl: "https://dzien.scooby.boo/fajniedzialek.mp4", gifFile: "fajniedzialek.gif" }, // Saturday
 ];
